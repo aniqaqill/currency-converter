bb84903fa895c3219839ca0a76249928cf0ed6e5
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2023-03-26 13:19:06 +0800

    added material angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatOptionModule } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
+
 
 import { AppComponent } from './app.component';
+import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyConverterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatOptionModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/currency-converter/currency-converter.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+interface ExchangeRate {
+  rates: Record<string, number>;
+  base: string;
+  date: string;
+}
+
+@Component({
+  selector: 'app-currency-converter',
+  templateUrl: './currency-converter.component.html',
+  styleUrls: ['./currency-converter.component.css']
+})
+export class CurrencyConverterComponent implements OnInit {
+
+  currencies = ['USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'HKD', 'NZD', 'SEK', 'SGD', 'ZAR','MYR'];
+  fromCurrency = 'MYR';
+  toCurrency = 'USD';
+  amount = 1;
+  result: number | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.convert();
+  }
+
+  convert() {
+    const url = `https://api.apilayer.com/exchangerates_data/convert?to=${this.toCurrency}&from=${this.fromCurrency}&amount=${this.amount}`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'apikey': '7DQ3YocaC8FfK4uHMil9sHb5FirWAEFP'
+    });
+    const options = { headers: headers };
+    this.http.get<any>(url, options).subscribe(response => {
+      this.result = response.result;
+    });
+  }
+  
+
+}
+
